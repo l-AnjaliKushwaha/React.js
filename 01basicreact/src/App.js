7a7dcac8e04d3cad3292ff5cb2f983c09dd681cd
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-01-27 11:17:24 +0530

    Day02

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,11 @@
+import Chai from "./Chai";
 
 function App() {
   return (
-   <h1>Chai Our React with create app | AK</h1>
+    <>
+   <h1>Chai Our React with create react app | AK</h1>
+   <Chai/>
+   </>
   );
 }
 
--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -1,8 +1,11 @@
-
+import Chai from "./chai";
 
 function App() {
   return (
+   <>
     <h1>Hello World with vite | AK</h1>
+    <Chai/>
+   </>
 
   );
 }
