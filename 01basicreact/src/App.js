e6c5113a2ba64cbaf8b4e89bef223759e4fcd3da
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-01-27 06:14:35 +0530

    Day01

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>Chai Our React with create app | AK</h1>
+  );
+}
+
+export default App;
---
+++ vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+  return (
+    <h1>Hello World with vite | AK</h1>
+
+  );
+}
+
+export default App
